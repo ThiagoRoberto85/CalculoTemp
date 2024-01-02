b4d584c5c4088bfcb59584c905c2621feea0ff82
Thiago Roberto <90941733+ThiagoRoberto85@users.noreply.github.com>
2024-01-02 11:26:12 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Temp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double C, F;
+            char repetir;
+
+            do
+            {
+                Console.Write("Digite a temperatura em Celcius por favor: ");
+                C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                F = 9.0 * C / 5.0 + 32.0;
+                Console.WriteLine("A temperatura equivalente em Fahrenheit é: " + F.ToString("F1", CultureInfo.InvariantCulture));
+                Console.Write("Deseja repetir? (Digite s ou n) : ");
+                repetir = char.Parse(Console.ReadLine());
+            } while (repetir == 's');
+
+            Console.ReadLine();
+        }
+    }
+}

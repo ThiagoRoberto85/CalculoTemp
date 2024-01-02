3ac7728d6a0dfa0e6e36d04212876b75a2dbcb30
Thiago Roberto <90941733+ThiagoRoberto85@users.noreply.github.com>
2024-01-02 11:41:06 -0500

    Teste

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@
                 C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
                 F = 9.0 * C / 5.0 + 32.0;
                 Console.WriteLine("A temperatura equivalente em Fahrenheit é: " + F.ToString("F1", CultureInfo.InvariantCulture));
-                Console.Write("Deseja repetir?? (Digite s ou n) : ");
+                Console.Write("Deseja repetir??? (Digite s ou n) : ");
                 repetir = char.Parse(Console.ReadLine());
             } while (repetir == 's');
 
